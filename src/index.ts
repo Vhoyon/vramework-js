4ff8b4ff2df22b6518d5bd3cba9813a12da09741
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-05-17 00:55:49 -0400

    Switch to using eslint to lint and format instead of editorconfig

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+console.log('Hello World');
+
+const testTest = 2;
+// eslint-disable-next-line no-unused-vars
+const testTemplateLiteral = `test`;
+
+if (testTest == 2) {
+	console.log('test');
+	console.log('ok');
+	
+	const ok: number[] = [];
+	
+	console.log(ok);
+	
+	(_tes: unknown): void => {
+		console.log('ok');
+	};
+}
