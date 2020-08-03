4319fddd5333923842861c66577a06cb84ed6851
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-08-03 16:28:15 -0400

    Tweak parameters to use explicit Type<T> declaration
    The CommandContainer now defines the generic type Type<T> which takes a class and force the given type to be a function that can be initialized of that type.
    
    The getLinkableCommandClass should now also more accurately distinguish from actual LinkableCommand classes and other objects.
    
    The createFromDir function now also allows to provide additional classes / folders to load via a new optional parameter.

--- src/api/commands/container/CommandContainer.ts
+++ src/api/commands/container/CommandContainer.ts
@@ -2,37 +2,39 @@ import path from 'path';
 import readdir from 'recursive-readdir';
 import LinkableCommand from '../LinkableCommand';
 
+type Type<T> = new (...args: unknown[]) => T;
+
 /**
  *
  * @param dirPath 	Path to the directory that contains the LinkableCommand classes.
  * 					Can be either a full path or a relative path (from the package.json location).
  * 					On Windows, the full path must have the drive letter lower cased.
  */
-async function getLinkableCommandClasses(dirPath: string): Promise<(new (...args: unknown[]) => LinkableCommand)[]> {
+async function getLinkableCommandClasses(dirPath: string): Promise<Type<LinkableCommand>[]> {
 	const fullDirPath = path.resolve(dirPath);
 	
 	const filePaths = await readdir(fullDirPath, ['!*.+(ts|js)']);
 	
-	const defaultImports = (await Promise.all(filePaths.map(async filePath => (await import(filePath)).default)));
+	const defaultImports: unknown[] = (await Promise.all(filePaths.map(async filePath => (await import(filePath)).default)));
 	
-	const commandClasses: (new (...args: unknown[]) => LinkableCommand)[] = defaultImports.filter(commandInstance => {
-		return commandInstance && commandInstance.prototype instanceof LinkableCommand;
-	});
+	const commandClasses: Type<LinkableCommand>[] = defaultImports.filter(commandInstance => {
+		return typeof commandInstance == 'function' && commandInstance.prototype instanceof LinkableCommand;
+	}).map(commandInstance => commandInstance as Type<LinkableCommand>);
 	
 	return commandClasses;
 }
 
 export class CommandContainer {
-	readonly links: ReadonlyMap<string, new (...args: unknown[]) => LinkableCommand>;
+	readonly links: ReadonlyMap<string, Type<LinkableCommand>>;
 	
-	private constructor(classes: ((new (...args: unknown[]) => LinkableCommand)[])) {
+	private constructor(classes: Type<LinkableCommand>[]) {
 		this.links = this.createLinkableCommandsMap(classes);
 	}
 	
-	protected createLinkableCommandsMap(classes: ((new (...args: unknown[]) => LinkableCommand)[])): Map<string, new (...args: unknown[]) => LinkableCommand> {
-		const linkMap = new Map<string, new (...args: unknown[]) => LinkableCommand>();
+	protected createLinkableCommandsMap(classes: Type<LinkableCommand>[]): Map<string, Type<LinkableCommand>> {
+		const linkMap = new Map<string, Type<LinkableCommand>>();
 		
-		const initLink = (link: string, linkClass: new (...args: unknown[]) => LinkableCommand): void => {
+		const initLink = (link: string, linkClass: Type<LinkableCommand>): void => {
 			linkMap.set(link, linkClass);
 		};
 		
@@ -56,11 +58,32 @@ export class CommandContainer {
 		return linkMap;
 	}
 	
-	static async createFromDir(dirPath: string): Promise<CommandContainer> {
-		return new this(await getLinkableCommandClasses(dirPath));
+	/**
+	 *
+	 * @param dirPath The path of the folder (relative or absolute) generate the links from.
+	 * @param additionalClasses Any additional folders / `LinkableCommand` classes to add to the container.
+	 */
+	static async createFromDir(dirPath: string, additionalClasses?: (Type<LinkableCommand> | string)[]): Promise<CommandContainer> {
+		const linkableTypes: Type<LinkableCommand>[] = [];
+		
+		linkableTypes.push(...await getLinkableCommandClasses(dirPath));
+		
+		if (additionalClasses) {
+			const additionalLinkableTypesArray = await Promise.all(additionalClasses.map(async additionalClass => {
+				if (typeof additionalClass == 'string') {
+					return await getLinkableCommandClasses(additionalClass);
+				} else {
+					return new Array(additionalClass);
+				}
+			}));
+			
+			additionalLinkableTypesArray.forEach(additionalLinkableTypes => linkableTypes.push(...additionalLinkableTypes));
+		}
+		
+		return new this(linkableTypes);
 	}
 	
-	static async createFromClasses(classes: ((new (...args: unknown[]) => LinkableCommand)[])): Promise<CommandContainer> {
+	static async createFromClasses(classes: Type<LinkableCommand>[]): Promise<CommandContainer> {
 		return new this(classes);
 	}
 }
