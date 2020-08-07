be36489062261dd80b737583936cddb0b933cd04
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-08-07 11:55:38 -0400

    Implement many baked in functions for AbstractBotCommand
    This allows for some quicker interactions in the commands action methods.
    
    A new DiscordCommand interface allows normalizing the functions of the commands.

--- src/api/commands/AbstractBotCommand.ts
+++ src/api/commands/AbstractBotCommand.ts
@@ -1,9 +1,10 @@
 import Discord from 'discord.js';
 import { VParsedCommand } from 'vcommand-parser';
 import CommandRouter from '../CommandRouter';
+import DiscordCommand from './DiscordCommand';
 import LinkableCommand from './LinkableCommand';
 
-export abstract class AbstractBotCommand extends LinkableCommand {
+export abstract class AbstractBotCommand extends LinkableCommand implements DiscordCommand {
 	request!: VParsedCommand;
 	router!: CommandRouter;
 	message!: Discord.Message;
@@ -18,15 +19,88 @@ export abstract class AbstractBotCommand extends LinkableCommand {
 		
 		this.client = this.message.client;
 	}
+	
+	getActualCall(): string {
+		return this.request.command!;
+	}
+	
+	getGuild(): Discord.Guild | null {
+		return this.message.guild;
+	}
+	
+	getTextChannel(): Discord.TextChannel | null {
+		const channel = this.getChannel();
+
+		return channel instanceof Discord.TextChannel ? channel : null;
+	}
+	
+	getChannel(): Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel {
+		return this.message.channel;
+	}
+	
+	/**
+	 * Retrieves the GuildMember that executed the request.
+	 */
+	getGuildMember(): Discord.GuildMember | null;
+	/**
+	 * Retrieves a GuildMember in the guild that this command was inputted in using a search criteria.
+	 *
+	 * @param user The member search criteria
+	 */
+	getGuildMember(user: Discord.UserResolvable): Discord.GuildMember | null;
+	
+	getGuildMember(user?: Discord.UserResolvable): Discord.GuildMember | null {
+		if (!user) {
+			return this.message.member;
+		}
+		
+		return this.getGuild()?.member(user) ?? null;
+	}
+	
+	/**
+	 * Retrieves the User that executed the request.
+	 */
+	getUser(): Discord.User;
+	/**
+	 * Retrieves a User in the guild that this command was inputted in using a search criteria.
+	 *
+	 * @param user The user search criteria
+	 */
+	getUser(user: Discord.UserResolvable): Discord.User | null;
+	
+	getUser(user?: Discord.UserResolvable): Discord.User | null {
+		if (!user) {
+			return this.message.author;
+		}
+		
+		return this.getGuild()?.member(user)?.user ?? null;
+	}
+	
+	sendMessage(text: string): Promise<Discord.Message> | null {
+		return this.getTextChannel()?.send(text) ?? null;
+	}
+	
+	sendMessageToUser(user: Discord.User, text: string): Promise<Discord.Message> {
+		return user.send(text);
+	}
+	
+	replyInDM(text: string): Promise<Discord.Message> | null {
+		const user = this.getUser();
+		
+		return user && this.sendMessageToUser(user, text);
+	}
+	
 	// getHelp(): string {
 	// 	throw new Error('Method not implemented.');
 	// }
-	// formatCommand(_commandToFormat: string): string {
-	// 	throw new Error('Method not implemented.');
-	// }
-	// formatOption(_optionToFormat: string): string {
-	// 	throw new Error('Method not implemented.');
-	// }
+	
+	formatCommand(commandToFormat: string): string {
+		return `${this.request.commandPrefix}${commandToFormat}`;
+	}
+	
+	formatOption(optionToFormat: string): string {
+		return `${this.request.optionPrefix}${optionToFormat}`;
+	}
 }
 
 export default AbstractBotCommand;
---
+++ src/api/commands/DiscordCommand.ts
@@ -0,0 +1,39 @@
+import Discord from 'discord.js';
+
+export interface DiscordCommand {
+	getGuild(): Discord.Guild | null;
+	
+	getTextChannel(): Discord.TextChannel | null;
+	
+	getChannel(): Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel;
+	
+	/**
+	 * Retrieves the GuildMember that executed the request.
+	 */
+	getGuildMember(): Discord.GuildMember | null;
+	/**
+	 * Retrieves a GuildMember in the guild that this command was inputted in using a search criteria.
+	 *
+	 * @param user The member search criteria
+	 */
+	getGuildMember(user: Discord.UserResolvable): Discord.GuildMember | null;
+	
+	/**
+	 * Retrieves the User that executed the request.
+	 */
+	getUser(): Discord.User;
+	/**
+	 * Retrieves a User in the guild that this command was inputted in using a search criteria.
+	 *
+	 * @param user The user search criteria
+	 */
+	getUser(user: Discord.UserResolvable): Discord.User | null;
+	
+	sendMessage(text: string): Promise<Discord.Message> | null;
+	
+	sendMessageToUser(user: Discord.User, text: string): Promise<Discord.Message>;
+	
+	replyInDM(text: string): Promise<Discord.Message> | null;
+}
+
+export default DiscordCommand;
