ede0150ee8f155633c808432b0fe21eded2e7219
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-08-07 00:27:19 -0400

    Add example on accessing option content from command's action function

--- src/test-commands/Command1.ts
+++ src/test-commands/Command1.ts
@@ -2,11 +2,11 @@ import AbstractBotCommand from '../api/commands/AbstractBotCommand';
 
 export default class Command1 extends AbstractBotCommand {
 	action(): void {
-		console.log('1');
+		console.log('Option content from command1 :', this.request.getOption('c')?.content);
 		// throw new Error('Method not implemented.');
 	}
 	
-	getCalls(): string | string[] {
+	getCalls(): string[] {
 		return ['command1', 'c'];
 	}
 }
